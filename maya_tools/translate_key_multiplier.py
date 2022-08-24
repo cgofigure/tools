af6557d0b808897d9c500a5b3551cd215e683ddd
cgofigure <chrisgofigan@gmail.com>
2022-08-23 20:18:50 -0500

    refactor_tools
    refactor pass on tools.

--- maya_tools/translate_key_multiplier.py
+++ maya_tools/translate_key_multiplier.py
@@ -1,9 +1,5 @@
 import maya.cmds as cmds
 
-def get_translate_multiplier(*args):
-    translate_multiplier = "1.0"
-    return translate_multiplier
-
 
 def get_key_range():
     rig_controls = cmds.ls(selection=True)
@@ -20,25 +16,21 @@ def get_key_range():
     return key_ranges
 
 
-def multiply_translate_keys():
-    multiplier = get_translate_multiplier()
+def multiply_translate_keys(value="1.0"):
     key_range = get_key_range()
     min_key = min(key_range)
     max_key = max(key_range)
-    first_key = int(min_key)
-    last_key = int(max_key)
     rig_controls = cmds.ls(selection=True)
     for rig_control in rig_controls:
-        cmds.selectKey(rig_control, time=(first_key, last_key), addTo=True, keyframe=True, attribute="translate")
-        x_keyframes = cmds.keyframe(rig_control + ".translateX", time=(first_key, last_key), query=True)
-        y_keyframes = cmds.keyframe(rig_control + ".translateY", time=(first_key, last_key), query=True)
-        z_keyframes = cmds.keyframe(rig_control + ".translateZ", time=(first_key, last_key), query=True)
+        x_keyframes = cmds.keyframe(rig_control + ".translateX", time=(int(min_key), int(max_key)), query=True)
+        y_keyframes = cmds.keyframe(rig_control + ".translateY", time=(int(min_key), int(max_key)), query=True)
+        z_keyframes = cmds.keyframe(rig_control + ".translateZ", time=(int(min_key), int(max_key)), query=True)
 
-        x_key_value = cmds.keyframe(rig_control + ".translateX", time=(first_key, last_key), query=True,
+        x_key_value = cmds.keyframe(rig_control + ".translateX", time=(int(min_key), int(max_key)), query=True,
                                     valueChange=True)
-        y_key_value = cmds.keyframe(rig_control + ".translateY", time=(first_key, last_key), query=True,
+        y_key_value = cmds.keyframe(rig_control + ".translateY", time=(int(min_key), int(max_key)), query=True,
                                     valueChange=True)
-        z_key_value = cmds.keyframe(rig_control + ".translateZ", time=(first_key, last_key), query=True,
+        z_key_value = cmds.keyframe(rig_control + ".translateZ", time=(int(min_key), int(max_key)), query=True,
                                     valueChange=True)
 
         if not x_keyframes:
@@ -46,16 +38,17 @@ def multiply_translate_keys():
         else:
             for x in range(0, len(x_keyframes)):
                 cmds.keyframe(rig_control + ".translateX", time=(x_keyframes[x], x_keyframes[x]),
-                              valueChange=(x_key_value[x] * float(multiplier)))
+                              valueChange=(x_key_value[x] * float(value)))
         if not y_keyframes:
             continue
         else:
             for y in range(0, len(y_keyframes)):
                 cmds.keyframe(rig_control + ".translateY", time=(y_keyframes[y], y_keyframes[y]),
-                              valueChange=(y_key_value[y] * float(multiplier)))
+                              valueChange=(y_key_value[y] * float(value)))
         if not z_keyframes:
             continue
         else:
             for z in range(0, len(z_keyframes)):
-                cmds.keyframe(rig_control + ".translateZ", time=(z_keyframes[z], z_keframes[z]),
-                              valueChange=(z_key_value[z] * float(multiplier)))
+                cmds.keyframe(rig_control + ".translateZ", time=(z_keyframes[z], z_keyframes[z]),
+                              valueChange=(z_key_value[z] * float(value)))
+    return
