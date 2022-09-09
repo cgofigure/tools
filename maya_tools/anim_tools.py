5a4dc817d4cc5295426bd3be0edc21b0f1f8ba2f
cgofigure <chrisgofigan@gmail.com>
2022-09-09 12:52:22 -0500

    refactor
    refactored

--- maya_tools/anim_tools.py
+++ maya_tools/anim_tools.py
@@ -1,6 +1,8 @@
 import maya.cmds as cmds
 
 def select_keyed():
+    """ Selects all keyed objects that are of the nodeType "camera' or "nurbsCurve"
+    """
     transform_nodes = cmds.ls(type="transform", recursive=True)
     cmds.select(clear=True)
 
@@ -8,9 +10,10 @@ def select_keyed():
         children = cmds.listRelatives(transform_node, children=True)
         child_node = cmds.nodeType(children[0])
 
-        if not children:
-            print("{} doesn't have a child object".format(transform_node))
         if child_node == "camera" or child_node == "nurbsCurve":
             keyed = cmds.keyframe(transform_node, query=True, keyframeCount=True)
             if keyed > 0:
                 cmds.select(transform_node, add=True)
+        else:
+            print("{}'s nodeType isn't camera or nubrsCurve".format(transform_node))
+
